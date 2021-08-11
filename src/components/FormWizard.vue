4d60540145f7ef748ed0276fcff2e92cc40cfc63
Hans Yadav <hansy@users.noreply.github.com>
2021-08-11 15:53:34 -0400

    add extra slot

--- src/components/FormWizard.vue
+++ src/components/FormWizard.vue
@@ -12,22 +12,25 @@
         <div class="wizard-progress-bar"
              :style="progressBarStyle"></div>
       </div>
-      <ul class="wizard-nav wizard-nav-pills" role="tablist" :class="stepsClasses">
-        <slot name="step" v-for="(tab, index) in tabs"
+      <div class="wizard-nav-container">
+        <ul class="wizard-nav wizard-nav-pills" role="tablist" :class="stepsClasses">
+          <slot name="step" v-for="(tab, index) in tabs"
               :tab="tab"
               :index="index"
               :navigate-to-tab="navigateToTab"
               :step-size="stepSize"
               :transition="transition">
-          <wizard-step :tab="tab"
+            <wizard-step :tab="tab"
                        :step-size="stepSize"
                        @click.native="navigateToTab(index)"
                        @keyup.enter.native="navigateToTab(index)"
                        :transition="transition"
                        :index="index">
-          </wizard-step>
-        </slot>
-      </ul>
+            </wizard-step>
+          </slot>
+        </ul>
+        <slot name="extra" v-bind="slotProps"></slot>
+      </div>
       <div class="wizard-tab-content">
         <slot v-bind="slotProps">
         </slot>
